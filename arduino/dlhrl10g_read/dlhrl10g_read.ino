9a18862984a8f6cba4010e93908cbfe5d2a5eda9
Ihab A.B. Awad <ihab.awad@gmail.com>
2017-12-26 20:42:12 -0800

    Fixed a bug in the script.

--- arduino/dlhrl10g_read/dlhrl10g_read.ino
+++ arduino/dlhrl10g_read/dlhrl10g_read.ino
@@ -1,8 +1,22 @@
-#include <Adafruit_BMP280.h>
+#include <TimerThree.h>
+
+#include <SparkFunTMP102.h>
 
 #include <Wire.h>
 extern "C" { 
 #include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
+
+bool debug = true;
+
+unsigned long timer_calls = 0;
+
+
+void log(const char* s) {
+  if (debug) {
+    Serial1.print("# ");
+    Serial1.println(s);
+  }
 }
 
 bool isBusy(byte status) {
@@ -10,7 +24,7 @@ bool isBusy(byte status) {
 }
 
 bool checkBusy(byte address) {
-  Wire.requestFrom(address, 1);
+  Wire.requestFrom(address, (byte)1);
   bool busy = isBusy(Wire.read());
   Wire.endTransmission();
   return busy;
@@ -24,9 +38,14 @@ void sendPressureSensorReadSingle(byte address) {
   Wire.endTransmission();
 }
 
-unsigned long readPressureSensor(byte address) {
-  while (checkBusy(address)) { }
-  Wire.requestFrom(address, 7);
+unsigned long readPressureSensor(byte bus, byte address) {
+  char msg[128];
+  while (checkBusy(address)) {
+    sprintf(msg, ". 0x%02x @ 0x%02x", bus, address);
+    log(msg);
+    delay(1);
+  }
+  Wire.requestFrom(address, (byte)7);
   Wire.read();  // status byte ignored
   byte b2 = Wire.read();  // pressure byte 2 (msb)
   byte b1 = Wire.read();  // pressure byte 1
@@ -86,15 +105,6 @@ void sendMuxChipSelect(byte muxAddress, byte chipNumber) {
   Wire.endTransmission();
 }
 
-void printValueGage(double value, double range, char symbol) {
-  double cnt = value / range * 1000.0;
-  Serial.print("|");
-  for (int i = 0; i < cnt; i++) {
-    Serial.print(symbol);
-  }
-  Serial.println();
-}
-
 void sendSentence(
      double baro,
      double temp,
@@ -110,6 +120,8 @@ void sendSentence(
   Serial1.print(dpAlpha);
   Serial1.print(",");
   Serial1.print(dpBeta);
+  // Serial1.print(" -- ");
+  // Serial1.print(timer_calls);
   Serial1.println();
 }
 
@@ -124,67 +136,105 @@ void tcaselect(uint8_t i) {
 }
 
 void tcascan() {
-    Serial.println("\nTCAScanner ready!");
+    Serial1.println("# TCAScanner ready!");
     
-    for (uint8_t t=0; t<8; t++) {
+    for (uint8_t t=0; t<5; t++) {
       tcaselect(t);
-      Serial.print("TCA Port #"); Serial.println(t);
- 
+      Serial1.print("# TCA Port #"); Serial1.println(t);
+
+      continue; // FOO
+      
       for (uint8_t addr = 0; addr<=127; addr++) {
         if (addr == TCAADDR) continue;
       
         uint8_t data;
         if (! twi_writeTo(addr, &data, 0, 1, 1)) {
-           Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
+           Serial1.print("#   Found I2C 0x");  Serial1.println(addr,HEX);
         }
       }
     }
-    Serial.println("\ndone");  
+    Serial1.println("# done");  
 }
 
-
-Adafruit_BMP280 bmp;
-
 void setup() {
-    while (!Serial1);
-    Wire.begin();
-    Serial1.begin(9600);
-    tcascan();
-    sendMuxChipSelect(0x70, 0x00);
-    bmp.begin();
+  while (!Serial1) {
+    delay(10);
+  }
+  Serial1.begin(9600);
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+  delay(10);
+
+  log("Serial established");    
+  Wire.begin();
+  log("Wire begin completed");
+  if (debug) { tcascan(); }
+
+/*
+  Timer3.initialize();
+  Timer3.setPeriod(500 * 1000);
+  Timer3.attachInterrupt(timerCallback);
+  */
+}
+
+const unsigned long period_ms = 500;
+unsigned long last_ms = 0;
+
+/*
+
+ISR(TIMER0_COMPA_vect) {
+  unsigned long current_ms = millis();
+  if (current_ms - last_ms > period_ms) {
+    timer_calls++;
+    last_ms = current_ms;
+    // Serial1.println("# signal timer0");    
+  }
+}
+
+*/
+
+void timerCallback() {
+  Serial1.println("# timer");
 }
 
 #define PASCALS_PER_IN_H2O 249.08
 #define PRESSURE_RANGE 10.0 * PASCALS_PER_IN_H2O
 
+TMP102 tempSensor(0x48);
+
 void loop() {
-  sendMuxChipSelect(0x70, 0x01);
+  sendMuxChipSelect(0x70, 0x00);
   sendPressureSensorReadSingle(0x29);
+
   sendMuxChipSelect(0x70, 0x02);
   sendPressureSensorReadSingle(0x29);
+
   sendMuxChipSelect(0x70, 0x03);
   sendPressureSensorReadSingle(0x29);
 
   sendMuxChipSelect(0x70, 0x00);
-  double p0 = bmp.readPressure();
-  double t = celsiusToKelvin(bmp.readTemperature());
-  
-  sendMuxChipSelect(0x70, 0x01);
   double dp0 = scalePressureReadingGage(
-      readPressureSensor(0x29),
-      PRESSURE_RANGE);
-  
-  sendMuxChipSelect(0x70, 0x02);
-  double dpA = scalePressureReadingDiff(
-      readPressureSensor(0x29),
-      PRESSURE_RANGE);
- 
-  sendMuxChipSelect(0x70, 0x03);
-  double dpB = scalePressureReadingDiff(
-      readPressureSensor(0x29),
+      readPressureSensor(0x00, 0x29),
       PRESSURE_RANGE);
 
-  sendSentence(p0, t, dp0, dpA, dpB);
+  sendMuxChipSelect(0x70, 0x02);
+  double dpAlpha = scalePressureReadingDiff(
+      readPressureSensor(0x02, 0x29),
+      PRESSURE_RANGE);
+
+  sendMuxChipSelect(0x70, 0x03);
+  double dpBeta = scalePressureReadingDiff(
+      readPressureSensor(0x03, 0x29),
+      PRESSURE_RANGE);
+
+  sendMuxChipSelect(0x70, 0x04);
+  double t = celsiusToKelvin(tempSensor.readTempC());
+
+  sendSentence(0, t, dp0, dpAlpha, dpBeta);
 
   delay(50);
 }
+
+void xloop() {}
+
