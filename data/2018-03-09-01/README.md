78f822c7c0c36737a28c4f0e11ea142b210dacdc
Ihab A.B. Awad <ihab.awad@gmail.com>
2018-03-13 18:36:05 -0700

    Data from test flight.

---
+++ data/2018-03-09-01/README.md
@@ -0,0 +1,6 @@
+# Data from flight test
+
+This was a flight test in a C172 targeting a peak altitude of 11,500
+feet. The barometric pressure sensor was installed. Due to an error of
+setup, the hoses to the yaw sensor were switched left/right from their
+usual orientation.
---
+++ data/2018-03-09-01/cleanup.sh
@@ -0,0 +1,9 @@
+# This file strips empty lines, status lines (with a "#"), and
+# lines with invalid data out of the raw log file. The log can
+# contain invalid lines because the writing process gets no
+# warning when the power is turned off!
+cat airball.log \
+    | grep -a -v '#' \
+    | grep -a -v '^$' \
+    | grep -a -v '[^0-9\.,-]' \
+    > plot.csv
