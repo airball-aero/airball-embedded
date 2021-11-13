0406dabc32e4f3ff2321d86e494f5e74c6d0b3ce
Ihab A.B. Awad <ihab.awad@gmail.com>
2021-11-13 15:37:02 -0800

    Fixed a bug in absolute value calculation.

--- firmware/airball_probe_esp32/pressures_to_airdata.cpp
+++ firmware/airball_probe_esp32/pressures_to_airdata.cpp
@@ -12,7 +12,7 @@ struct airdata_triple_struct pressures_to_airdata(const calibration_surface* cs_
 
   float rpa = dpa / dp0;
   float rpb = dpb / dp0;
-  float rpb_pos = math.abs(rpb);
+  float rpb_pos = fabs(rpb);
   float signb = rpb < 0 ? -1.0 : 1.0;
   
   r.alpha = interpolate(cs_alpha, rpa, rpb_pos, err);
