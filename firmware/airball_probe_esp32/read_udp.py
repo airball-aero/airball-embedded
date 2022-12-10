74d0733d08c0b956ee65f68f2e66de0bf271019b
Ihab Awad <ihab.awad@gmail.com>
2022-12-09 19:54:33 -0800

    Simple program to read UDP.

---
+++ firmware/airball_probe_esp32/read_udp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 30123
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message: %s" % data)
